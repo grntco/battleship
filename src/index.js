12997d476bdb1289d900b4af56fc92d4e9eb0ebe
Grant Collins <grantcollins03@gmail.com>
2023-11-15 11:02:18 -0500

    created nav.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import "./styles.css";
 import { mainContainer } from "./ui/mainContainer";
+import { nav } from "./ui/nav";
 
+document.body.appendChild(nav);
 document.body.appendChild(mainContainer);
---
+++ src/ui/nav.js
@@ -0,0 +1,4 @@
+const nav = document.createElement('nav');
+nav.className = 'nav';
+
+export { nav };
